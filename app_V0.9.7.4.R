53367f07dfec41eb6301d34b667730c9062d0340
theojohn <yannistheologidis@gmail.com>
2022-07-01 11:49:26 +0300

    Update app_V0.9.7.4.R

--- app_V0.9.7.4.R
+++ app_V0.9.7.4.R
@@ -1,13 +1,3 @@
-#http://shiny.rstudio.com/
-#https://s3.amazonaws.com/assets.datacamp.com/production/course_3805/slides/chapter3.pdf--------------------- explore data with shiny
-#https://yang-tang.github.io/shinyjqui/
-#https://github.com/dreamRs/esquisse----------------------------------------------------------for Data Visualisation
-# https://emitanaka.org/shinycustomloader/----------------------------------------------------spinner
-# https://goodekat.github.io/redres/articles/redres-vignette.html#shiny-app-----------------evaluate LMMs with shiny
-# https://rdrr.io/cran/lme4/man/findbars.html ----------------------------------findbars: Determine random-effects expressions from a formula
-# https://www.stat.uga.edu/sites/default/files/FactorsInR.pdf
-# https://hosting.analythium.io/run-shiny-apps-locally/ ----------------------------------- hosting Shiny apps
-
 library(shiny)
 library(shinythemes)
 library(plyr)
